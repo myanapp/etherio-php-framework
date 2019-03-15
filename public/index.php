5353150d8bf29dfadb93bb4be0fca7c07e8639fb
myanapp <git@myan.app>
2019-03-15 23:02:48 +0630

    auto commited

--- public/index.php
+++ public/index.php
@@ -7,3 +7,10 @@
  */
 
 $server = require_once __DIR__ . '/../app/autoload.php';
+
+$host = 'ezc2-50-19-109-120.compute-1.amazonaws.com';
+$port = '5432';
+$dbname = 'da5mp219si4bfv';
+$uname = 'otjeumaqejyauk';
+$pword = '647ddb59dfa2a0549add230481405146f89511eb6cca74991db6e6e8a45e82e5';
+$db = pg_connect("host=$host port=$port dbname=$dbname user=$uname password=$pword") or die('cannot connect to Database!!');
---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git add .
+git commit -am "auto commited"
+git push heroku master
+heroku open
