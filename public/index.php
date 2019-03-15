5a110d08124d2728d51d8565fe0b298e350abacf
myanapp <git@myan.app>
2019-03-15 23:10:48 +0630

    auto commited

--- public/index.php
+++ public/index.php
@@ -13,10 +13,6 @@ ini_set('html_errors', false);
 
 $server = require_once __DIR__ . '/../app/autoload.php';
 
-$dbname = 'da5mp219si4bfv';
-$username = 'otjeumaqejyauk';
-$password = '647ddb59dfa2a0549add230481405146f89511eb6cca74991db6e6e8a45e82e5';
-
-$db = new PDO('pgsql:host=ezc2-50-19-109-120.compute-1.amazonaws.com:5432;dbname=da5mp219si4bfv', $username, $password, '');
+$db = pg_connect('pgsql://otjeumaqejyauk:647ddb59dfa2a0549add230481405146f89511eb6cca74991db6e6e8a45e82e5@ezc2-50-19-109-120.compute-1.amazonaws.com:5432/dbname=da5mp219si4bfv', $username, $password);
 
 print_r($db);
